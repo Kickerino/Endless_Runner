6509925f431a7177c8485debae68a7c2a35401a0
DizzyShark351 <k.hulst@outlook.com>
2015-09-01 12:55:20 +0200

    Intro upload + Unity Startup
    Intro upload + Unity Startup

---
+++ Tests_01-09-15/Assets/Scripts/Video.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class Video : MonoBehaviour {
+
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+		GetComponent<AudioSource>().clip = movie.audioClip;
+		movie.Play();
+		GetComponent<AudioSource>().Play();
+	}
+}
